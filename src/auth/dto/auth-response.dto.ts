e6857ef48becbd044ab2b90b176c53ca0d457476
contactochiga <contactochiga@gmail.com>
2025-09-21 15:59:57 +0100

    Update auth-response.dto.ts

--- src/auth/dto/auth-response.dto.ts
+++ src/auth/dto/auth-response.dto.ts
@@ -1,7 +1,4 @@
-// src/auth/dto/auth-response.dto.ts
-import { User } from '../../user/entities/user.entity';
-
 export class AuthResponseDto {
   user: Pick<User, 'id' | 'email' | 'role'>;
-  token: string; // keep this name consistent with AuthService
+  accessToken: string;
 }
