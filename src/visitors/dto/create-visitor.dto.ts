68cc4bc0c3a7244bd0b04771c2953d9578f097bf
contactochiga <contactochiga@gmail.com>
2025-09-22 16:09:25 +0100

    Update create-visitor.dto.ts

--- src/visitors/dto/create-visitor.dto.ts
+++ src/visitors/dto/create-visitor.dto.ts
@@ -1,6 +1,15 @@
 // src/visitors/dto/create-visitor.dto.ts
+import { IsString, IsOptional } from 'class-validator';
+
 export class CreateVisitorDto {
+  @IsString()
   name!: string;
-  purpose!: string;
-  time!: string;
+
+  @IsOptional()
+  @IsString()
+  purpose?: string;
+
+  @IsOptional()
+  @IsString()
+  time?: string; // optional, e.g. "14:00"
 }
