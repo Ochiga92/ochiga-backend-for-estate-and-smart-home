e5d2f4ddeadc311529ae63709fc90f81061fa0bb
contactochiga <contactochiga@gmail.com>
2025-09-23 06:03:10 +0100

    Update iot.module.ts

--- src/iot/iot.module.ts
+++ src/iot/iot.module.ts
@@ -1,14 +1,16 @@
+// src/iot/iot.module.ts
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { IotController } from './iot.controller';
 import { IotService } from './iot.service';
 import { Device } from './entities/device.entity';
 import { DeviceLog } from './entities/device-log.entity';
+import { IotGateway } from './iot.gateway';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Device, DeviceLog])],
   controllers: [IotController],
-  providers: [IotService],
-  exports: [IotService], // in case we need it elsewhere
+  providers: [IotService, IotGateway], // ✅ Register both service + gateway
+  exports: [IotService, IotGateway],   // ✅ Allow usage in other modules if needed
 })
 export class IotModule {}
